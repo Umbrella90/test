1831b8ad03c65ab3d9b58d948c2f3b3cfce9d319
Umbrella90 <54934903+Umbrella90@users.noreply.github.com>
2024-03-15 16:17:04 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 def test(a: int, b: int) -> int:
   retrun a + b
 
+def test2(a: int) -> int:
+  return a ** 2
+  
 print(test(5, 5))
 print(test(0, 0))
 print(test(1, 1))
+print(test(2, 2) == test2(2))
