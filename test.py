2e3bbd19ede7c77e8abd89b83cd9e78031b483dc
Umbrella90 <54934903+Umbrella90@users.noreply.github.com>
2024-03-15 17:20:25 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,11 @@ def test(a: int, b: int) -> int:
 
 def test2(a: int) -> int:
   return a ** 2
-  
+
+def test3(a: int, b: int) -> int:
+  return a - b
 print(test(5, 5))
 print(test(0, 0))
 print(test(1, 1))
 print(test(2, 2) == test2(2))
+print(test(3, 5))
