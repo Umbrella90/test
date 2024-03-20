8224a299322bee65a8c77d6c923c8a8ee9943515
Umbrella90 <54934903+Umbrella90@users.noreply.github.com>
2024-03-20 15:58:37 +0300

    Update test.py

--- test.py
+++ test.py
@@ -6,9 +6,10 @@ def svz_exp(a: int, b: int = 2) -> int:
 
 def svz_multy(a: int, b: int) -> int:
   return a * b
-print(test(5, 5))
-print(test(0, 0))
-print(test(1, 1))
-print(test(2, 2) == test2(2))
-print(test(3, 5))
-print(test(-1, 1))
+
+print(svz_sum(5, 5))
+print(svz_sum(0, 0))
+print(svz_sum(1, 1))
+print(svz_sum(2, 2) == svz_exp(2))
+print(svz_sum(3, 5))
+print(svz_sum(-1, 1))
