70e05c8e2bef89fff7cada8a5819c28a50d26637
Umbrella90 <54934903+Umbrella90@users.noreply.github.com>
2024-03-20 16:05:09 +0300

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,9 @@ def svz_exp(a: int, b: int = 2) -> int:
 def svz_multy(a: int, b: int) -> int:
   return a * b
 
+def svz_div(a: int, b: int) -> int:
+  return a / b
+
 print(svz_sum(5, 5))
 print(svz_sum(0, 0))
 print(svz_sum(1, 1))
